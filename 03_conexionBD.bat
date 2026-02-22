84a0c0e12e078d180d1c45f4b7eee1446fdd3a87
Jeferson-Arias <Jefersonariasa@hotmail.com>
2026-02-22 14:43:59 -0500

    Configuración adicionar .bat

--- 03_conexionBD.bat
+++ 03_conexionBD.bat
@@ -1,4 +1,12 @@
 @echo off
+
+:waitMariaDB
+docker exec mariadb_db mariadb -u root -proot123 -e "USE entidadesTerritorialesColombia; SHOW TABLES;" >nul 2>&1
+if errorlevel 1 (
+    timeout /t 3 /nobreak >nul
+    goto waitMariaDB
+)
+
 echo.
 echo Comprobando extension MySQL Client...
 code --list-extensions | findstr /I "cweijan.vscode-mysql-client2" >nul 2>&1
